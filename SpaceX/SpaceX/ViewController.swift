c5c63c8fc5397c9d7ab06cda1d60730bd04807e8
Mikita Sh <gentled.druzhinnik@gmail.com>
2022-02-06 02:18:48 +0300

    init: start project

---
+++ SpaceX/SpaceX/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpaceX
+//
+//  Created by Mikita Shalima on 6.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
