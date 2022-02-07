3e467d9b778d6afb6b3a502f047056db6ecb2ace
Mikita Sh <gentled.druzhinnik@gmail.com>
2022-02-07 15:46:50 +0300

    feat: setup constraints for collectionView in LaunchpadList

--- SpaceX/SpaceX/Controllers/Launchpads List/LaunchpadListVC.swift
+++ SpaceX/SpaceX/Controllers/Launchpads List/LaunchpadListVC.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class LaunchpadListVC: UIViewController {
-
+    
     private let flowLayout = UICollectionViewFlowLayout()
     private lazy var collectionView: UICollectionView = {
         let collection = UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
@@ -19,12 +19,22 @@ class LaunchpadListVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         addViews()
+        setupConstraints()
     }
     
     func addViews() {
         view.addSubview(collectionView)
     }
-
+    
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
+    }
+    
 }
