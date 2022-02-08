55f793fef2c2419c94d601284c1ab24a2234aa26
Mikita Sh <gentled.druzhinnik@gmail.com>
2022-02-08 12:58:57 +0300

    feat: setup collectionView in RocketList

--- SpaceX/SpaceX/Controllers/Rockets List/RocketListVC.swift
+++ SpaceX/SpaceX/Controllers/Rockets List/RocketListVC.swift
@@ -20,6 +20,9 @@ class RocketListVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
         addViews()
         setupConstraints()
     }
@@ -36,5 +39,43 @@ class RocketListVC: UIViewController {
             collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
         ])
     }
+    
+}
+
+// MARK: - UICollectionViewDataSource
+extension RocketListVC: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+       
+        let cell = UICollectionViewCell()
+        return cell
+    }
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+extension RocketListVC: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let paddingWidth: CGFloat = 18 * 2
+        let availableWidth = collectionView.frame.width - paddingWidth
+        return  CGSize(width: availableWidth, height: availableWidth)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        
+        UIEdgeInsets(top: 30, left: 18, bottom: 30, right: 18)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        50
+    }
+}
+
+//MARK: - UICollectionViewDelegate
+extension RocketListVC: UICollectionViewDelegate {
     
 }
