9d3ad988af2a031a845bdce31269b29b35673c22
Mikita Sh <gentled.druzhinnik@gmail.com>
2022-02-08 12:53:48 +0300

    feat: add collectionView in RocketList

--- SpaceX/SpaceX/Controllers/Rockets List/RocketListVC.swift
+++ SpaceX/SpaceX/Controllers/Rockets List/RocketListVC.swift
@@ -9,12 +9,22 @@ import UIKit
 
 class RocketListVC: UIViewController {
 
+    private let flowLayout = UICollectionViewFlowLayout()
+    private lazy var collectionView: UICollectionView = {
+        let collection = UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
+        collection.translatesAutoresizingMaskIntoConstraints = false
+        collection.backgroundColor = UIColor(named: "Queen Blue")
+        return collection
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        addViews()
     }
     
-
+    func addViews() {
+        view.addSubview(collectionView)
+    }
 
 }
