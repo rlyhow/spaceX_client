4396459787361569402db804c93a8ac63fcfd4df
Mikita Sh <gentled.druzhinnik@gmail.com>
2022-02-09 14:34:16 +0300

    feat: add LauchpadDetailVC

---
+++ SpaceX/SpaceX/Controllers/Launchpads List/LauchpadDetailVC.swift
@@ -0,0 +1,24 @@
+//
+//  LauchpadDetailVC.swift
+//  SpaceX
+//
+//  Created by Mikita Shalima on 9.02.22.
+//
+
+import UIKit
+
+class LauchpadDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.white
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        tabBarController?.tabBar.isHidden = true        
+    }
+
+}
