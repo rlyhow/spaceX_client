1cdd2a2e8287d660936826f92e278280914fb015
Mikita Sh <gentled.druzhinnik@gmail.com>
2022-02-10 13:55:30 +0300

    feat: add header block in rocketDetail

--- SpaceX/SpaceX/Controllers/Rockets List/RocketDetailVC.swift
+++ SpaceX/SpaceX/Controllers/Rockets List/RocketDetailVC.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class RocketDetailVC: UIViewController {
-
+    
     var rocketObject: Rocket?
     
     var mainScrollView: UIScrollView = {
@@ -16,6 +16,24 @@ class RocketDetailVC: UIViewController {
         scroll.translatesAutoresizingMaskIntoConstraints = false
         scroll.showsVerticalScrollIndicator = false
         return scroll
+    }()
+    
+    lazy var headerBlock: RocketDetailHeaderImageView = {
+        let imageView = RocketDetailHeaderImageView(frame: .zero)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.setup(image: rocketObject?.rocketImage, rocketName: rocketObject?.name)
+        return imageView
+    }()
+    
+    var mainStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 30
+        stackView.alignment = .fill
+        stackView.isLayoutMarginsRelativeArrangement = true
+        stackView.layoutMargins = UIEdgeInsets(top: 40, left: 20, bottom: 40, right: 20)
+        return stackView
     }()
     
     override func viewDidLoad() {
@@ -35,6 +53,8 @@ class RocketDetailVC: UIViewController {
     
     func addViews() {
         view.addSubview(mainScrollView)
+        mainScrollView.addSubview(headerBlock)
+        mainScrollView.addSubview(mainStackView)
     }
     
     func addConstraints() {
@@ -43,7 +63,18 @@ class RocketDetailVC: UIViewController {
             mainScrollView.topAnchor.constraint(equalTo: view.topAnchor),
             mainScrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             mainScrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            headerBlock.leadingAnchor.constraint(equalTo: mainScrollView.leadingAnchor),
+            headerBlock.topAnchor.constraint(equalTo: mainScrollView.topAnchor),
+            headerBlock.trailingAnchor.constraint(equalTo: mainScrollView.trailingAnchor),
+            headerBlock.heightAnchor.constraint(equalTo: headerBlock.widthAnchor),
+            
+            mainStackView.leadingAnchor.constraint(equalTo: mainScrollView.leadingAnchor),
+            mainStackView.topAnchor.constraint(equalTo: headerBlock.bottomAnchor),
+            mainStackView.trailingAnchor.constraint(equalTo: mainScrollView.trailingAnchor),
+            mainStackView.bottomAnchor.constraint(equalTo: mainScrollView.bottomAnchor),
+            mainStackView.centerXAnchor.constraint(equalTo: mainScrollView.centerXAnchor),
         ])
     }
-
+    
 }
---
+++ SpaceX/SpaceX/Views/Blocks For Detail Pages/RocketDetailHeaderImageView.swift
@@ -0,0 +1,50 @@
+//
+//  RocketDetailHeaderImageView.swift
+//  SpaceX
+//
+//  Created by Mikita Shalima on 10.02.22.
+//
+
+import UIKit
+
+class RocketDetailHeaderImageView: UIImageView {
+    
+    private let rocketNameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = UIColor(named: "White")
+        label.font = UIFont(name: "Roboto-Bold", size: 48)
+        label.lineBreakMode = .byCharWrapping
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addViews()
+        addConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func addViews() {
+        addSubview(rocketNameLabel)
+    }
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            rocketNameLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            rocketNameLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -30),
+            rocketNameLabel.trailingAnchor.constraint(lessThanOrEqualTo: trailingAnchor, constant: -20),
+        ])
+    }
+    
+    func setup(image: UIImage?, rocketName: String?) {
+        self.image = image
+        self.contentMode = .scaleAspectFill
+        self.rocketNameLabel.text = rocketName
+    }
+    
+}
