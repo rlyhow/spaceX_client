93c7a84b910f159176950f8ab9bc2f1f1d1dea79
Mikita Sh <gentled.druzhinnik@gmail.com>
2022-04-12 20:11:41 +0300

    fix: implement the correct display of navBar and tabBar for ios 15*

--- SpaceX/SpaceX/AppDelegate.swift
+++ SpaceX/SpaceX/AppDelegate.swift
@@ -9,28 +9,57 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        if #available(iOS 15.0, *) {
+            let navigationBarAppearance = UINavigationBarAppearance()
+            navigationBarAppearance.configureWithOpaqueBackground()
+            navigationBarAppearance.backgroundColor = UIColor(named: "Queen Blue")
+            navigationBarAppearance.titleTextAttributes = [.font: UIFont(name: "Roboto-Bold", size: 24)!, .foregroundColor: UIColor(named: "White")!]
+            UINavigationBar.appearance().standardAppearance = navigationBarAppearance
+            UINavigationBar.appearance().compactAppearance = navigationBarAppearance
+            UINavigationBar.appearance().scrollEdgeAppearance = navigationBarAppearance
+        }
+        
+        
+        if #available(iOS 15, *) {
+            let tabBarAppearance = UITabBarAppearance()
+            tabBarAppearance.backgroundColor = UIColor(named: "Queen Blue")
+            tabBarAppearance.stackedLayoutAppearance.selected.titleTextAttributes = [.foregroundColor: UIColor(named: "Coral")!]
+            tabBarAppearance.stackedLayoutAppearance.normal.titleTextAttributes = [.foregroundColor: UIColor(named: "Champagne")!]
+            
+            tabBarAppearance.stackedLayoutAppearance.selected.iconColor = UIColor(named: "Coral")!
+            tabBarAppearance.stackedLayoutAppearance.normal.iconColor = UIColor(named: "Champagne")!
+            
+            UITabBar.appearance().standardAppearance = tabBarAppearance
+            UITabBar.appearance().scrollEdgeAppearance = tabBarAppearance
+        } else {
+            UITabBarItem.appearance().setTitleTextAttributes([.foregroundColor: UIColor(named: "Coral")!], for: .selected)
+            UITabBarItem.appearance().setTitleTextAttributes([.foregroundColor: UIColor(named: "Champagne")!], for: .normal)
+            UITabBar.appearance().barTintColor = UIColor(named: "Queen Blue")
+        }
+        
+        
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
-
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
+    
+    
 }
 
